8b408c459e7e0d6c74a7000590e344f888cf5f07
Oliver Lietz <olli@apache.org>
2022-07-17 21:01:57 +0200

    use Hamcrest's asserThat

--- src/test/java/org/apache/sling/installer/hc/OsgiInstallerHealthCheckTest.java
+++ src/test/java/org/apache/sling/installer/hc/OsgiInstallerHealthCheckTest.java
@@ -22,9 +22,10 @@ import java.util.List;
 import java.util.Map;
 
 import org.hamcrest.Matchers;
-import org.junit.Assert;
 import org.junit.Test;
 import org.osgi.framework.Version;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class OsgiInstallerHealthCheckTest {
 
@@ -32,7 +33,7 @@ public class OsgiInstallerHealthCheckTest {
     public void testParseEntityIdsWithVersions() {
         String[] entityIdsAndVersions = new String[] { "idA 1.0.0", "idA 2.0.0", "idB" };
         Map<String, List<Version>> map = OsgiInstallerHealthCheck.parseEntityIdsWithVersions(entityIdsAndVersions);
-        Assert.assertThat(map, Matchers.allOf(
+        assertThat(map, Matchers.allOf(
                 Matchers.aMapWithSize(2),
                 Matchers.hasEntry("idA", Arrays.asList(new Version("1.0.0"), new Version("2.0.0"))),
                 Matchers.hasEntry("idB", null)));
